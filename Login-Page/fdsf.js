67fc6b68f2a34e5e99a820b01effc79a9f6d80ed
prakash-006 <167277911+prakash-006@users.noreply.github.com>
2025-06-11 18:50:34 +0530

    Add files via upload

---
+++ Login-Page/fdsf.js
@@ -0,0 +1,9 @@
+let upbtn = document.querySelector('.signupbtn')
+let inbtn = document.querySelector('.signinbtn')
+let nameField = document.querySelector('.namefield')
+let tittle = document.querySelector('.tittle')
+let underline = document.querySelector('.underline')
+
+inbtn.addEventListener('click',()=>{
+    nameField.style.maxheight = '0';
+})
